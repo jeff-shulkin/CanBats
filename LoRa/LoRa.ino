865624f2939c742fbc64d6353472fbdfeb566c9f
tsholtis <tsholtis@umich.edu>
2023-10-10 18:26:42 -0400

    Moved LoRa Github libraries to local files. Added send and receive functions in .ino file

--- LoRa/LoRa.ino
+++ LoRa/LoRa.ino
@@ -1,40 +1,78 @@
-#include <LoRa.h>
+//#include <LoRa.h>
+
+
+#include "LoRa.h"
 #include <SPI.h>
 
-// TODO: look Arduino SPI pinout pins 
 #define LORA_MISO 12
 #define LORA_MOSI 11
 #define LORA_SCK  13
 #define LORA_NSS  10
-#define REG_RSSI_WIDEBAND        0x2c
+
+void test_receive();
+void test_send();
 
 
 char buff[] = "Hello";
 int buff_size = 5;
 uint8_t response = 0xFF;
+int counter = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LORA_MISO, INPUT);
   pinMode(LORA_MOSI, OUTPUT);
   pinMode(LORA_SCK, OUTPUT);
-  pinMode(LORA_NSS, OUTPUT);
-
+//  pinMode(LORA_NSS, OUTPUT);
+  
   Serial.begin(9600);
 
-  SPI.begin();
+  Serial.println("LoRa Receiver");
+  //LoRa.begin(915E6);
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  //SPI.begin();
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(buff);
-  response = 0xFF;
-  SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(LORA_NSS, LOW);
-  SPI.transfer(REG_RSSI_WIDEBAND);
-  response = SPI.transfer(0x00);
-  digitalWrite(LORA_NSS, HIGH);
-  SPI.endTransaction();
-  Serial.println(response);
-  
+  test_receive();
+  test_send();
+}
+
+void test_receive(){
+    // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
+
+void test_send(){
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
 }
