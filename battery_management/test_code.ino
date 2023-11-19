a84b0e85387182ecdbc8ad8a155ed99b14167d0b
ahkassem <kassemal@umich.edu>
2023-11-19 04:58:37 -0500

    major bms test fixes

--- battery_management/test_code.ino
+++ battery_management/test_code.ino
@@ -6,11 +6,14 @@
 #define MAC_MFG_STATUS 0x57
 #define GAUGING 0x21
 #define PARAMS_ADDR 0x4685
+#define MAC_BUFF_SIZE (32)
 
 void setup() {
     Serial.begin(9600);
 
-    uint16_t mfg_status = get_mfg_status();
+    uint8_t data_buff[MAC_BUFF_SIZE];
+    mac_read_command(data_buff, MAC_BUFF_SIZE, MAC_MFG_STATUS);
+    uint16_t mfg_status = data_buff[0] | (data_buff[1]<<8);
     
     uint8_t gauging_en = (mfg_status>>3) & 1;
     
@@ -32,42 +35,70 @@ void toggle_gauging() {
     Wire.endTransmission();
 }
 
-int get_mfg_status() {
+void mac_read_command(uint8_t *data, uint8_t data_capacity, uint16_t subcmd) {
   // Request ManufacturingStatus()
+  if (data_capacity < MAC_BUFF_SIZE) {
+    Serial.println("Data capacity not met");
+    return;
+  }
   Serial.println("Sending I2C message to get Manufacturing Status");
 
   Wire.beginTransmission(BMS_ADDR);
   Wire.write(MAC_SUBCMD);   // MAC starting address
-  Wire.write(MAC_MFG_STATUS);   // send low byte of mfg status cmd
-  Wire.write(MAC_MFG_STATUS >> 8);  // send high byte of mfg status cmd
+  Wire.write(subcmd);   // send low byte of mfg status cmd
+  Wire.write(subcmd >> 8);  // send high byte of mfg status cmd
   Wire.endTransmission();
 
   // Get ManufacturingStatus()
-  Wire.requestFrom(BMS_ADDR, 6);
-  while (Wire.available() < 6) {
-    Serial.println("Waiting to receive 6 bytes");
+  Wire.requestFrom(BMS_ADDR, MAC_BUFF_SIZE+4);
+  while (Wire.available() < MAC_BUFF_SIZE+4) {
+    Serial.println("Waiting to receive %d bytes", MAC_BUFF_SIZE+4);
     delay(1000);
   }
 
   Serial.println("Bytes received");
 
-  uint16_t cmd_check = Wire.read() + (Wire.read() << 8);
+  uint8_t mac_buff[MAC_BUFF_SIZE+4];
+
+  for (uint8_t i=0; i<MAC_BUFF_SIZE+4; ++i) {
+    mac_buff[i] = Wire.read();
+  }
+
+  uint16_t cmd_check = mac_buff[0] | (mac_buff[1] << 8);
 
   char strbuf[50];
   sprintf(strbuf, "The command sent was %#04x, we received %#04x", MAC_MFG_STATUS, cmd_check);
   Serial.println(strbuf);
+  
+  uint8_t len = mac_buff[35];
+  uint8_t checksum = mac_buff[34];
 
-  uint16_t manufacturing_status = Wire.read() + (Wire.read() << 8);
-  sprintf(strbuf, "Raw manufacturing_status val: %#04x");
-  Serial.println(strbuf);
+  uint8_t validate = mac_buff[0]+mac_buff[1];
+  for(int i = 0; i < len - 4; ++i)
+  {
+    validate += mac_buff[i+2];
+    data[i] = mac_buff[i+2];
+  }
 
-  Wire.read();  // NOTE: STILL NEED TO CHECK THE CHECKSUM
+  if(checksum != (~validate))
+  {
+    Serial.println("Check Sum failed for data");
+    return;
+  }
 
-  uint8_t len = Wire.read();    // the size of the message is also transmitted
-  Serial.print("Expected len 6, got");
-  Serial.println(len);
+  // // this is for a two byte command
+  // // TODO: abstract code, use length
+  // uint16_t manufacturing_status = Wire.read() + (Wire.read() << 8);
+  // sprintf(strbuf, "Raw manufacturing_status val: %#04x");
+  // Serial.println(strbuf);
 
-  return manufacturing_status;
+  // Wire.read();  // NOTE: STILL NEED TO CHECK THE CHECKSUM
+
+  // uint8_t len = Wire.read();    // the size of the message is also transmitted
+  // Serial.print("Expected len 6, got");
+  // Serial.println(len);
+
+  // return manufacturing_status;
 }
 
 void bms_setup() {
